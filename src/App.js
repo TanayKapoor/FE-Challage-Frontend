efadb6b78d9f6cfe394c40a4df03a419cec8e445
Tanay Kapoor <tanay.kapoor@cginfinity.com>
2023-03-11 21:20:18 +0530

    Started Proj

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Dashboard from './Components/Dashboard/dashboard';
 
-function App() {
+const Card = ({ title, content }) => (
+  <div className='card'>
+    <h2>{title}</h2>
+    <p>{content}</p>
+  </div>
+);
+
+let data = null;
+
+const App = () => {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch('http://localhost:3000/tasks')
+      .then(res => res.json())
+      .then(data => setData(data))
+      .then(data => console.log(data));
+  }, []);
+
+  // convert data to array
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Dashboard>
+        {[...new Array(data && data.length)].map((_, i) => (
+          <Card
+            // use id in data as key
+            key={data && data[i].id}
+            title={`{data.id} ${i + 1}`}
+            content={data && data.map(task => <p>{task.Status}</p>)}
+          />
+        ))}
+      </Dashboard>
     </div>
   );
-}
+};
 
 export default App;
